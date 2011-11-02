42d36964f46c9c9c475e99bf8f8a90581cd08da2
mattn <mattn.jp@gmail.com>
2011-11-02 11:51:45 +0900

    add java template.

---
+++ template/java/base-main.java
@@ -0,0 +1,5 @@
+public class {{_name_}} {
+	public static void main(String[] args) {
+		{{_cursor_}}
+	}
+}
---
+++ template/java/snip-try-catch-finally.java
@@ -0,0 +1,6 @@
+try {
+	{{_cursor_}}
+} catch (Throwable e) {
+	e.printStackTrace();
+} finally {
+}

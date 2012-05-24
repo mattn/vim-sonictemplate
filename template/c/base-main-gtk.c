e9259c19a2e71f918d0b3021dd8d310ac7dae3b4
mattn <mattn.jp@gmail.com>
2012-05-24 23:29:52 +0900

    add base-main-gtk

---
+++ template/c/base-main-gtk.c
@@ -0,0 +1,19 @@
+#include <gtk/gtk.h>
+
+int
+main(int argc, char* argv[]) {
+	GtkWidget* window;
+
+	gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "helloworld");
+    g_signal_connect(G_OBJECT(window), "delete-event", gtk_main_quit, window);
+
+	{{_cursor_}}
+
+    gtk_widget_show_all(window);
+
+	gtk_main();
+	return 0;
+}

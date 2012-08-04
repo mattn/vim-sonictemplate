a33c8cb60bffb2fe207cca7f2855899270299fb6
Katsunori Kanda <potix2@gmail.com>
2012-08-04 21:43:52 +0900

    add test template for python

---
+++ template/python/base-test.py
@@ -0,0 +1,15 @@
+"""
+{{_name_}}
+"""
+
+import unittest
+
+class {{_expr_:substitute('{{_input_:name}}', '\w\+', '\u\0', '')}}(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_something(self):
+        {{_cursor_}}

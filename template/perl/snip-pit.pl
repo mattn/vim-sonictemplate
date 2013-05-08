5a5811173d429d24e3e497ebd5e8f0ef8c7fc714
mattn <mattn.jp@gmail.com>
2013-05-08 21:31:42 +0900

    Use _var_

--- template/perl/snip-pit.pl
+++ template/perl/snip-pit.pl
@@ -1,7 +1,7 @@
 use Config::Pit;
 
 my $config = pit_get("{{_input_:domain}}", require => {
-	"username" => "username of {{_input_:domain}}",
-	"password" => "password of {{_input_:domain}}",
+	"username" => "username of {{_var_:domain}}",
+	"password" => "password of {{_var_:domain}}",
 });
 {{_cursor_}}
--- template/ruby/snip-pit.rb
+++ template/ruby/snip-pit.rb
@@ -1,7 +1,7 @@
 require "pit"
 
 config = Pit.get("{{_input_:domain}}", :require => {
-	"username" => "username of {{_input_:domain}}",
-	"password" => "password of {{_input_:domain}}",
+	"username" => "username of {{_var_:domain}}",
+	"password" => "password of {{_var_:domain}}",
 })
 {{_cursor_}}

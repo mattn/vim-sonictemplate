1bd840e315cedcbd66b1e8b461898271073cf717
mattn <mattn.jp@gmail.com>
2013-07-31 21:23:03 +0900

    Add jUnit tempalte for java

---
+++ template/java/base-test.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * {{_name_}}
+ */
+public class {{_name_}} {
+	@Test
+	public void test{{_input_:func}}() {
+		{{_cursor_}}
+	}
+
+	@Before
+	public void setup() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+}
+{{_filter_:test}}
---
+++ template/java/snip-test-func.java
@@ -0,0 +1,4 @@
+@Test
+public void test{{_input_:func}}() {
+	{{_cursor_}}
+}

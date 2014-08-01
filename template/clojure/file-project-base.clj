5dcf055a7845dec5657aa78d55736593347c2336
mattn <mattn.jp@gmail.com>
2014-08-01 11:45:07 +0900

    Add clojure template

---
+++ template/clojure/file-project-base.clj
@@ -0,0 +1,11 @@
+(defproject {{_expr_:expand('%:p:h:t')}} "0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://"
+  :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
+  :repositories {"name" "http://"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "0.9.2"]]
+  :main ^:skip-aot {{_expr_:expand('%:p:h:t')}}
+  :target-path "target/%s"
+  :min-lein-version "2.2.0")
+

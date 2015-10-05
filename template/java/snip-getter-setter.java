1383d7ad494dc8aedc071eeff1b28f1b672f87eb
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-10-06 02:03:46 +0900

    fix getter/setter template

--- template/java/snip-getter-setter.java
+++ template/java/snip-getter-setter.java
@@ -2,7 +2,7 @@
  * get {{_input_:name}}
  * @return {{_var_:name}}
  */
-public {{_var_:name}} get{{_expr_:substitute('{{_var_:name}}', '\w\+', '\u\0', '')}}() {
+public {{_input_:type}} get{{_expr_:substitute('{{_var_:name}}', '\w\+', '\u\0', '')}}() {
 	return {{_var_:name}};
 }
 
@@ -10,6 +10,6 @@ public {{_var_:name}} get{{_expr_:substitute('{{_var_:name}}', '\w\+', '\u\0', '
  * set {{_var_:name}}
  * @param {{_var_:name}}
  */
-public void set{{_expr_:substitute('{{_var_:name}}', '\w\+', '\u\0', '')}}({{_var_:name}} {{_var_:name}}) {
+public void set{{_expr_:substitute('{{_var_:name}}', '\w\+', '\u\0', '')}}({{_var_:type}} {{_var_:name}}) {
 	this.{{_var_:name}} = {{_var_:name}};
 }

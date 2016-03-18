48865a94526ef82547e225093465763e83bdae46
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-03-18 18:48:55 +0900

    Add gin-gonic

---
+++ template/go/base-web-gin.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mattn/go-colorable"
+)
+
+func main() {
+	gin.DefaultWriter = colorable.NewColorableStderr()
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+	r.Run()
+}

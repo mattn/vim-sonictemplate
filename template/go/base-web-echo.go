92b1dcb99d778d4c1c1b0e18d552302816871306
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-06-22 14:52:01 +0900

    add template for WAF/echo

---
+++ template/go/base-web-echo.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "{{_cursor_}}")
+	})
+	e.Logger.Fatal(e.Start(":8989"))
+}

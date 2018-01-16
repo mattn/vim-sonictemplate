4005ee94f1e7123864ace071042cdc3a6fb0829f
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-01-17 00:53:00 +0900

    fix indentation

--- template/go/base-web-standard.go
+++ template/go/base-web-standard.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "{{_cursor_}}")
 	})
-    http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
 {{_filter_:web-standard}}

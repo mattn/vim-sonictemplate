e2daafe834343c1a3565c4435f7c0e3b7a96f88b
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-01-26 01:34:24 +0900

    Add elixer

---
+++ template/elixer/base-main.exs
@@ -0,0 +1,5 @@
+defmodule {{_name_}} do
+    def {{_input_:func}} do
+        IO.puts "{{_cursor_}}"
+    end
+end
---
+++ template/erlang/base-main.erl
@@ -0,0 +1,5 @@
+-module({{_name_:main}}).
+-export([{{_name_:main}}/0]).
+
+{{_name_:main}}() ->
+  io:put_chars("{{_cursor_}}").

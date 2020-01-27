6b7ab55384011e7478724d24fb99ad1aa2550114
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-01-27 11:37:36 +0900

    Add nocomment

---
+++ template/java/base-class-nocomment.java
@@ -0,0 +1,7 @@
+{{_lang_util_:package}}
+
+public class {{_expr_:substitute('{{_name_}}', '\w\+', '\u\0', '')}} {
+	public {{_expr_:substitute('{{_name_}}', '\w\+', '\u\0', '')}}() {
+		{{_cursor_}}
+	}
+}
---
+++ template/java/base-main-nocomment.java
@@ -0,0 +1,7 @@
+{{_lang_util_:package}}
+
+public class {{_name_}} {
+	public static void main(String[] args) {
+		{{_cursor_}}
+	}
+}
